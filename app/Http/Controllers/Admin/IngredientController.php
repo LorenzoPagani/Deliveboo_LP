1983db27d2d47edd425ea5ca39d3543c073a58d7
Giulia Ruggeri <giuliaruggeri94@hotmail.com>
2024-02-20 17:35:33 +0100

    Fix controller

--- app/Http/Controllers/Admin/IngredientController.php
+++ app/Http/Controllers/Admin/IngredientController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
-class RestaurantController extends Controller
+class IngredientController extends Controller
 {
     /**
      * Display a listing of the resource.
