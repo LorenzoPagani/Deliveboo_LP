115de9bf29c1a5509deafc3f2e952593267f3238
Giulia Ruggeri <giuliaruggeri94@hotmail.com>
2024-02-20 17:10:05 +0100

    Routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController; //<---- Import del controller precedentemente creato!
+use App\Http\Controllers\DishesController;
+use App\Http\Controllers\IngredientsController;
+use App\Http\Controllers\RestaurantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +30,9 @@ Route::middleware(['auth'])
         // - il percorso "/" diventa "admin/"
         // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('dishes', [DishesController::class]);
+        Route::resource('ingredients', [IngredientsController::class]);
+        Route::resource('restaurants', [RestaurantController::class]);
     });
 
 require __DIR__ . '/auth.php';
