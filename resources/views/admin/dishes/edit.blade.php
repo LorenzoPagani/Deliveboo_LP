b3bada18e1cd67dcb06435f0cef4221b6cae5afa
Ilaria Salvati <ilariasalvati00@gmail.com>
2024-02-23 12:26:29 +0100

    aggiunti errori di validazione

--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -7,32 +7,62 @@
             @method('PUT')
             <div class="form-group">
                 <label for="name">Dish name</label>
-                <input type="text" class="form-control" id="name" name="name" value="{{ $dish->name }}">
+                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ $dish->name }}">
+                @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="picture">Dish picture</label>
-                <input type="text" class="form-control" id="picture" name="picture" placeholder="Enter picture URL">
+                <input type="text" class="form-control @error('picture') is-invalid @enderror" id="picture" name="picture" placeholder="Enter picture URL">
+                @error('picture')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="description">Dish description</label>
-                <textarea class="form-control" id="description" name="description" rows="3">{{ $dish->description }}</textarea>
+                <textarea class="form-control @error('description') is-invalid @enderror"  id="description" name="description" rows="3">{{ $dish->description }}</textarea>
+                @error('description')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="price">Dish price</label>
-                <input type="text" class="form-control" id="price" name="price" value="{{ $dish->price }}">
+                <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ $dish->price }}">
+                @error('price')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="visible">Dish visibility</label>
-                <select class="form-control" id="visible" name="visible">
+                <select class="form-control @error('price') is-invalid @enderror" id="visible" name="visible">
                     <option value="1" {{ $dish->visible == 1 ? 'selected' : '' }}>Visible</option>
                     <option value="0" {{ $dish->visible == 0 ? 'selected' : '' }}>Not visible</option>
                 </select>
+                @error('visible')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="ingredients">Ingredients</label>
-                <textarea class="form-control" name="ingredients" id="ingredients" cols="30" rows="10"
+                <textarea class="form-control @error('ingredients') is-invalid @enderror" name="ingredients" id="ingredients" cols="30" rows="10"
                     placeholder="enter ingredients">
                 </textarea>
+                @error('ingredients')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
             </div>
             <button type="submit" class="btn btn-primary">Edit</button>
         </form>
