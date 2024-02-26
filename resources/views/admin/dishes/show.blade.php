f531ead34aeeedf58b058cb65f7f7d0b5b09bbc4
Lorenzo Pagani <larrypg2@gmail.com>
2024-02-26 16:44:08 +0100

    adds style to show.blade.php

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -1,17 +1,63 @@
 @extends('layouts.admin')
 @section('content')
-    <div class="container">
-        <h1>Show Dish</h1>
-        <div class="col-md-4">
-            <div class="card mt-2">
-                <div class="card-header text-center">{{ $dish->name }}</div>
-                <div class="card-body d-flex flex-column align-items-center">
-                    <img src="{{ $dish->picture }}" alt="photo">
-                    <p class="mt-2">{{ $dish->description }}</p>
-                    <p class="mt-2">ingredients: {{ $dish->ingredients }}</p>
-                    <a class="btn btn-primary" href="{{ route('admin.dishes.index') }}">Menù</a>
+    <div class="container text-center">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-6">
+                <div class="card mt-2">
+                    <div class="card-header text-center">
+                        <h3>{{ $dish->name }}</h3>
+                    </div>
+                    <div class="card-body">
+                        <img class="w-50 mb-2" src="{{ $dish->picture }}" alt="photo">
+                        <div class="card-header rounded">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">{{ $dish->description }}</li>
+                                <li class="list-group-item">{{ $dish->ingredients }}</li>
+                                <li class="list-group-item"><a class="btn btn-primary"
+                                        href="{{ route('admin.dishes.edit', $dish->id) }}">Modifica</a>
+                                    <a class="btn btn-primary" href="{{ route('admin.dishes.index') }}">Torna al Menù</a>
+                                </li>
+                            </ul>
+                        </div>
+
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <style lang="scss">
+        body {
+            background-color: #c5d7dd;
+        }
+
+        .card {
+            background-image: url(https://img.freepik.com/free-photo/handrawn-barbecue-elements_125540-588.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1708646400&semt=ais);
+            margin-bottom: 2rem;
+
+            .card-header {
+                box-shadow: 0px 3px 2px 0px rgb(0 0 0 / 30%), 0 2px 4px -1px rgb(7 7 7 / 69%);
+
+            }
+
+            .card-header,
+            .list-group-item {
+                background-color: #FF960C;
+                color: white;
+            }
+
+            .list-group {
+                border-radius: 10px;
+
+            }
+
+            .card-body {
+
+                img {
+                    border-radius: 50%;
+                }
+            }
+
+        }
+    </style>
 @endsection
