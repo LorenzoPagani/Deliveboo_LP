91eda88d703b411326f6de15490a8bc9b952eeb0
Giulia Ruggeri <giuliaruggeri94@hotmail.com>
2024-02-26 19:41:11 +0100

    Min fix

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -74,39 +74,4 @@
 
             }
         </style>
-        {{-- <a class="text-right" href="{{ route('admin.dishes.create') }}">Add Dish</a>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Dish Name</th>
-                            <th>Price</th>
-                            <th>Picture</th>
-                            <th>description</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($dishes as $dish)
-                            <tr>
-                                <td>{{ $dish->name }}</td>
-                                <td>{{ $dish->price }}</td>
-                                <td><img src="{{ $dish->picture }}" style="width=100px;height:100px" alt="photo"></td>
-                                <td>{{ $dish->description }}</td>
-                                <td>
-                                    <a href="{{ route('admin.dishes.show', $dish->id) }}" class="btn btn-primary">View</a>
-                                    <a href="{{ route('admin.dishes.edit', $dish->id) }}" class="btn btn-warning">Edit</a>
-                                    <form style="display:inline-block" method="POST"
-                                        action="{{ route('admin.dishes.destroy', $dish->id) }}">
-                                        <input type="hidden" name="_method" value="DELETE">
-                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <button type="submit" class="form-control btn btn-danger">Delete</a>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table> --}}
-    </div>
-    </div>
-    </div>
 @endsection
