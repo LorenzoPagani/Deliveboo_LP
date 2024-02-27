198b791d996803eba9a91d76c2fcae2e4cb00eb2
Giulia Ruggeri <giuliaruggeri94@hotmail.com>
2024-02-27 17:14:25 +0100

    Aggiunge dati ai seeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -22,6 +22,42 @@ class TypeSeeder extends Seeder
                 'name' => 'ristorante italiano',
                 "ristorante" => [1, 2, 3]
             ],
+            [
+                'name' => 'trattoria',
+                'ristorante' => [4, 7],
+            ],
+            [
+                'name' => 'osteria',
+                'ristorante' => [5, 8],
+            ],
+            [
+                'name' => 'ristorante di pesce',
+                'ristorante' => [6, 9],
+            ],
+            [
+                'name' => 'pasta fresca',
+                'ristorante' => [7],
+            ],
+            [
+                'name' => 'cucina toscana',
+                'ristorante' => [8],
+            ],
+            [
+                'name' => 'ristorante moderno',
+                'ristorante' => [8, 9],
+            ],
+            [
+                'name' => 'enoteca',
+                'ristorante' => [10],
+            ],
+            [
+                'name' => 'cucina tradizionale',
+                'ristorante' => [4, 5, 6, 8],
+            ],
+            [
+                'name' => 'cucina cinese',
+                'ristorante' => [11],
+            ],
         ];
 
         foreach ($categories as $category) {
