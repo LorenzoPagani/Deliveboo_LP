e171e28fd9ffb0796748d255724755fcc9afa6ab
Alessio-Tosoni <alessiotosoni988@gmail.com>
2024-03-01 15:45:03 +0100

    implementa box messaggio errore compilazione form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,6 +10,15 @@
                     <div class="card-body">
                         <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
                             @csrf
+                            @if ($errors->any())
+                    	        <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
 
                             <div class="mb-4 row">
                                 <label for="name"
