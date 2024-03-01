df2de3d5c235575bb11eca86dd3d2a80c328dabd
Giulia Ruggeri <giuliaruggeri94@hotmail.com>
2024-03-01 14:41:44 +0100

    Aggiunge modale e cambia layout cards

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,20 @@ import.meta.glob(['../img/**'])
 
 document.querySelectorAll('.delete-btn').forEach(button => {
     button.addEventListener('submit', function (e) {
-        const confirmDelete = confirm(
-            'Sei sicuro di voler eliminare questo elemento?'
-        )
-        if (!confirmDelete) {
-            e.preventDefault() // Previeni l'invio del modulo se l'utente annulla l'eliminazione
-        }
+        
+        e.preventDefault()
     })
+
 })
+
+var myModal = document.getElementById('deleteModal')
+let confirmDeleteButton = document.getElementById('confirmDelete')
+
+myModal.addEventListener('shown.bs.modal', function () {
+    confirmDeleteButton.addEventListener('click',function(e){
+        document.getElementById('deleteForm').submit();
+        myModal.modal('hide');
+    })
+
+})
+
