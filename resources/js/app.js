fb1bf10e3dce682627e2425bfe26b4085f83bb3b
Giulia Ruggeri <giuliaruggeri94@hotmail.com>
2024-03-12 09:05:11 +0100

    Min fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,16 +3,16 @@ import '~resources/scss/app.scss'
 import * as bootstrap from 'bootstrap'
 import.meta.glob(['../img/**'])
 
-document.querySelectorAll('.delete-btn').forEach(button => {
-    button.addEventListener('submit', function (e) {
-        const confirmDelete = confirm(
-            'Sei sicuro di voler eliminare questo elemento?'
-        )
-        if (!confirmDelete) {
-            e.preventDefault() // Previeni l'invio del modulo se l'utente annulla l'eliminazione
-        }
-    })
-})
+// document.querySelectorAll('.delete-btn').forEach(button => {
+//     button.addEventListener('submit', function (e) {
+//         const confirmDelete = confirm(
+//             'Sei sicuro di voler eliminare questo elemento?'
+//         )
+//         if (!confirmDelete) {
+//             e.preventDefault() // Previeni l'invio del modulo se l'utente annulla l'eliminazione
+//         }
+//     })
+// })
 
 /* aggiunta preview foto nei form */
 picture.onchange = evt => {
